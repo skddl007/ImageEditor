075c7469660d6f0775d800fb83835f35b3e00891
SANDEEP KUMAR <143518095+skddl007@users.noreply.github.com>
2024-03-31 17:46:56 +0530

    Update ImageEditor.py

--- ImageEditor.py
+++ ImageEditor.py
@@ -6,18 +6,10 @@ import os
 import cv2
 import numpy as np
 import pytesseract
-from ctypes import pythonapi, py_object
+import tempfile
 
 app = Flask(__name__, static_folder='data')
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-
-# Define the _Py_HashDouble function using the provided example
-def custom_Py_HashDouble(obj, value):
-    hash_func = pythonapi._Py_HashDouble
-    hash_func.restype = py_object
-    return hash_func(obj, value)
-
-
+app.secret_key = "This is secret"
 
 # stack class
 undostack = []
@@ -31,14 +23,14 @@ def rotate(main_image):
     rotate_angle = int(request.form['rotate'])
     image = main_image.rotate(rotate_angle)
 
-def crop(main_image,x,y,x0,y0,crop_size):
-    if x0>=x and y0>=y:
-        crop_left  = x
-        crop_upper = y
-        crop_right = x + crop_size
-        crop_lower = y + crop_size
-        image=main_image.crop((crop_left, crop_upper, crop_right, crop_lower))
-    return image
+# def crop(main_image,x,y,x0,y0,crop_size):
+#     if x0>=x and y0>=y:
+#         crop_left  = x
+#         crop_upper = y
+#         crop_right = x + crop_size
+#         crop_lower = y + crop_size
+#         image=main_image.crop((crop_left, crop_upper, crop_right, crop_lower))
+#     return image
 
 def blur(main_image,blur_amount):
     if blur_amount>100:
@@ -58,6 +50,7 @@ def detect_faces(main_image):
     pil_image = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))  # Convert back to PIL Image
     return pil_image
 
+
 def extract_text_from_image(image_data):
     try:
         image = Image.open(BytesIO(image_data))
@@ -65,14 +58,26 @@ def extract_text_from_image(image_data):
         return text
     except Exception as e:
         return str(e)
+    
+# dawonload
 
-#----------------------------------------------------------------------------------------------------------------------------------------
-# def img_to_base64(image_data):
-#     img_io = BytesIO()
-#     image_data.save(img_io, format='JPEG')  # Save the image in JPEG format
-#     img_io.seek(0)
-#     image_data = base64.b64encode(img_io.getvalue()).decode('utf-8')
-#     return image_data
+@app.route('/download')
+def download():
+    try:
+        if len(undostack) > 0:
+            # Get the last edited image from the stack
+            edited_image = undostack[-1]
+            # Save the edited image to a temporary file
+            temp_file = tempfile.NamedTemporaryFile(delete=False, suffix='.jpg')
+            edited_image.save(temp_file.name, format='JPEG')
+
+            # Serve the temporary file for download
+            return send_file(temp_file.name, as_attachment=True)
+        else:
+            # If no edited image is available, redirect to the portal
+            return redirect(url_for('portal'))
+    except Exception as e:
+        return render_template('image_editor.html')
 
 #------------------------------------------------------------------ROUTE-----------------------------------------------------------------
 
@@ -89,77 +94,120 @@ def portal():
             return redirect(url_for('editor'))
     except Exception as e:
         print(e)
+        # undostack=[]
+        # redostack=[]
     return render_template('image_inpu.html')
 
+
+# Route for the console page
 @app.route('/editor', methods=['GET', 'POST'])
 def editor():
-    blur_amount = 0
-    image_data = None  # Initialize image_data outside of the try block
     try:
+        blur_amount=0
+        unducount='0'
+        reducount='0'  
+        total=0
+        messages=''
+        # image_data=None # initialize image_data to None
+        
         if len(undostack) > 0:
-            image_data = undostack[-1]
+            image_data = undostack[-1]  # Get the last uploaded image from the stack
             img_io = BytesIO()
-            image_data.save(img_io, format='JPEG')
-            img_io.seek(0)
-            image_data = base64.b64encode(img_io.getvalue()).decode('utf-8')
-
+            image_data.save(img_io, format='JPEG')  # Save the image in JPEG format
+            img_io.seek(0)  # Move the cursor to the beginning of the BytesIO object
+            image_data = base64.b64encode(img_io.getvalue()).decode('utf-8')   # Encode the image data in base64 format and decode it to a UTF-8 string
+            
             if request.method == 'POST':
                 effect = request.form['effect']
+                print(effect)
                 main_image = Image.open(BytesIO(base64.b64decode(image_data)))
-                image = None  # Initialize image variable
-                
+                    
                 if effect == 'rotate':
                     rotate_angle = int(request.form['rotate'])
                     image = main_image.rotate(rotate_angle)
+                    # undustack append
+                    undostack.append(image)
 
                 elif effect == 'crop':
                     crop_size = int(request.form['crop_size'])
                     x0, y0 = main_image.size
-                    x = int(request.form['x'])
-                    y = int(request.form['y'])
+                    x=int(request.form['x'])
+                    y=int(request.form['y'])
                     
-                    if x0 >= x and y0 >= y:
-                        crop_left = x
+                    # Calculate the crop box to maintain center crop
+                    if x0>=x and y0>=y:
+                        crop_left  = x
                         crop_upper = y
                         crop_right = x + crop_size
                         crop_lower = y + crop_size
+                        # Crop the image
                         image = main_image.crop((crop_left, crop_upper, crop_right, crop_lower))
-
-                elif effect == 'blur':
+                        # undustack append
+                        undostack.append(image)
+                
+                elif effect=='blur':
                     blur_amount = int(request.form['blur'])
-                    blur_amount = blur_amount % 100
+                    blur_amount=blur_amount%100
                     image = main_image.filter(ImageFilter.BoxBlur(blur_amount))
-
+                    # undustack append
+                    undostack.append(image)
                 elif effect == 'filter':
                     filter_name = request.form.get('effect_option')
-                    lst = ["BLUR", "CONTOUR", "DETAIL", "EDGE_ENHANCE", "EDGE_ENHANCE_MORE", "EMBOSS", "FIND_EDGES", "SMOOTH", "SMOOTH_MORE", "SHARPEN"]
+                    print(filter_name)
+                    lst=["BLUR", "CONTOUR", "DETAIL", "EDGE_ENHANCE", "EDGE_ENHANCE_MORE","EMBOSS", "FIND_EDGES", "SMOOTH", "SMOOTH_MORE", "SHARPEN"]
                     lst_filter = [ImageFilter.BLUR, ImageFilter.CONTOUR, ImageFilter.DETAIL, ImageFilter.EDGE_ENHANCE, 
                                   ImageFilter.EDGE_ENHANCE_MORE, ImageFilter.EMBOSS, ImageFilter.FIND_EDGES, ImageFilter.SMOOTH, ImageFilter.SMOOTH_MORE, ImageFilter.SHARPEN]
-
+                    
+                    # image = main_image
                     if filter_name in lst:
-                        filter_obj = lst_filter[lst.index(filter_name)]
+                        # Applying the blur filter
+                        filter_obj = lst_filter[lst.index(filter_name)]    # Get the ImageFilter object
                         image = main_image.filter(filter_obj)
-
+                    # undustack append
+                    undostack.append(image)
+                    
                 elif effect == 'detect_faces':
                     image = detect_faces(main_image)
-
+                    # undustack append
+                    undostack.append(image)
+                
                 elif effect == 'extract_text':
                     text = extract_text_from_image(base64.b64decode(image_data))
                     flash("Extracted Text: " + text)
+                       
+                elif effect == 'undu':
+                    if len(undostack) > 0:
+                        image = undostack.pop()
+                        redostack.append(image)
+                        print('redu', len(redostack))
 
-                if image is not None:
+                elif effect == 'redu' and len(redostack) > 0:
+                    image = redostack.pop()
                     undostack.append(image)
+                    print('undu', len(undostack))
+
+                elif effect == 'reset':
+                    image = undostack[0]
+                    undostack.clear()
+                    redostack.clear()
+                    undostack.append(image)
+                    
+                if image is not None:
+                    # another stuff
                     edited_io = BytesIO()
                     image.save(edited_io, format='JPEG')
                     edited_io.seek(0)
                     image_data = base64.b64encode(edited_io.getvalue()).decode('utf-8')
-
-            return render_template('image_editor.html', image_data=image_data, blur_amount=blur_amount)
+                    # length of list
+                    unducount=len(undostack)
+                    reducount=len(redostack)
+                    total=unducount+reducount
+                    
+            return render_template('image_editor.html', image_data=image_data,blur_amount=blur_amount,unducount=unducount,total=total,messages=messages)
         else:
-            return render_template('image_inpu.html', image_data=image_data)
-    except Exception as e:
-        print(e)
-        return 'Error processing image'
+            return render_template('image_inpu.html')
+    except:
+        return render_template('image_editor.html',image_data=image_data,unducount=unducount,total=total)
 
 if __name__ == '__main__':
     app.run(debug=True)
